e2a4978a6e44e6cf6ba0309f4fe05e41a632244d
david-belinsky-sbu <dabelinsky@cs.stonybrook.edu>
2020-09-17 14:43:41 -0400

    Finalized version of Tumor superpatch extractor.

--- Tumor_Patch_Extractor/extract_patches.py
+++ Tumor_Patch_Extractor/extract_patches.py
@@ -35,6 +35,7 @@ seed = int(sys.argv[11])
 random.seed(seed)
 #manifest = {'Slide': [], 'Patch': [], 'Tumor': [], 'Annotation': [], 'Annotation Discrete': [], 'Native Scale': []}
 manifest = pd.DataFrame(columns = ['Slide', 'Patch', 'Tumor', 'Annotation', 'Annotation Discrete', 'Native Scale'])
+manifest.set_index(['Slide', 'Patch'], drop=False)
 #manifest = pd.read_csv(datafile)
 manifest["Native_Scale"] = 0
 
@@ -58,6 +59,10 @@ def find(pattern, path, remotefile):
                 return os.path.join(path, name), remotefile
     return downRemoteFile('/home/tcga_all/{}/{}'.format(path.split('/')[-1], pattern), remotefile)
 
+def whiteness(patch):
+    wh = np.mean(np.std(patch, axis=(0,1)))
+    return wh
+
 patch_size_40X = 1600
 level = 0
 missing_slides = []
@@ -73,13 +78,16 @@ if not os.path.exists(outputDir): os.makedirs(outputDir)
 
 file_list = glob.glob(annotdata + '*.png')
     
-for fname in file_list:    
+for fname in file_list:
+    #seed += 1    
     slide_name = os.path.basename(fname)
     slide_name = slide_name[:-4] + '.svs'
+    print(slide_name)
     #output_file = os.path.join(outputDir, slide_name + '_' + patchloc + '.png' )
     #tumorloc = os.path.join(svsloc, tumor)
 
     try:
+    #if True:
         if not os.path.exists(tumordir):
             if tumor not in missing_tumors:
                 missing_tumors.append(tumor)
@@ -100,66 +108,101 @@ for fname in file_list:
         else:
             print('[WARNING] mpp value not found. Assuming it is 40X with mpp=0.254!', slide_name);
             mag = 10.0 / float(0.254);
-    
         scale = 40.0 / mag;  # scale patch size from 'mag' to 40x
-        pw = int(patch_size_40X / scale)
-    
+        pw = int(round((patch_size_40X / scale)))
         width = oslide.dimensions[0];
         height = oslide.dimensions[1];
-
         highcount = 0
         medcount = 0
         lowcount = 0
         xmax = width // pw
         ymax = height // pw
-
-        annot = cv2.imread(fname)
-        pwannot = pw / 20
-        print('annotation shape: {}'.format(str(annot.shape)))
-        print('original shape: {}, {}'.format(width, height))
-        print('annotpwidth: {}, patchwidth: {}'.format(pwannot, pw))
-
+        slidename = slide_name.split('.')[-3]
+        annot = cv2.imread(fname, 0)
+        annotscale = width / annot.shape[1]
+        pwannot = int(round(pw / annotscale))
+        #print('annotation shape: {}'.format(str(annot.shape)))
+        #print('original shape: {}, {}'.format(width, height))
+        #print('annotpwidth: {}, patchwidth: {}'.format(pwannot, pw))
+        #random.seed(seed)
+        dupecount = 0
+        randomcount = 0
         while highcount < high or medcount < med or lowcount < low:
             #Get random Patches
+            if dupecount > 50:
+                break
+            if randomcount > 10000:
+                break
+            randomcount += 1
             x = random.randint(0, xmax - 1)
             y = random.randint(0, ymax - 1)
-            
-
-        #if int(patchlocs[0]) + pw > width:
-        #    pw_x = width - int(patchlocs[0])
-        #else:
-        #    pw_x = pw
-        #if int(patchlocs[1]) + pw > height:
-        #    pw_y = height - int(patchlocs[1])
-        #else:
-        #    pw_y = pw
-
-        patch = oslide.read_region((int(patchlocs[0]), int(patchlocs[1])), 0, (pw_x, pw_y));
-        if pw_x != pw and pw_y != pw:
-            patch.resize((int(patch_size_40X * pw_x / pw), int(patch_size_40X * pw_y / pw)), Image.ANTIALIAS)
-        else:
-            patch.resize((int(patch_size_40X), int(patch_size_40X)), Image.ANTIALIAS)
-        patch.save(output_file)
-        manifest.at[index, 'Native_Scale'] = mag
+            annoty = (y * pwannot) + 1
+            annotx = (x * pwannot) + 1
+            annotpatch = annot[annoty: annoty + pwannot, annotx: annotx + pwannot]
+            totaltumor = np.sum(annotpatch != 0)
+            annotport = (totaltumor * 1.0) / (pwannot * pwannot)
+            try:
+            #if True:
+                if annotport > 0.75:
+                    if highcount < high:
+                        patch = oslide.read_region(((x*pw)+1, (y*pw)+1), 0, (pw, pw));
+                        patcharr = np.array(patch)
+                        if (patcharr[:,:,3].max() == 0):
+                            #print('blank: Patch: {}_{}'.format((x*pw)+1, (y*pw)+1))
+                            continue
+                        elif whiteness(patcharr[:,:,:3]) < 12:
+                            #print('white: Patch: {}_{}'.format((x*pw)+1, (y*pw)+1))
+                            continue
+                        patch.resize((int(patch_size_40X), int(patch_size_40X)), Image.ANTIALIAS)
+                        manifest = manifest.append(pd.DataFrame({'Slide': [slidename], 'Patch': ['{}_{}'.format((x*pw)+1, (y*pw)+1)], 'Tumor': [tumor], 'Annotation': [annotport], 'Annotation Discrete': [1], 
+                                                            'Native Scale': [mag]}).set_index(["Slide", "Patch"], drop=False), verify_integrity=True)
+                        patch.save('{}{}_{}_{}.png'.format(outputDir, slidename, (x*pw)+1, (y*pw)+1))
+                        highcount += 1
+                        print('highcount: {}, Patch: {}_{}'.format(highcount, (x*pw)+1, (y*pw)+1))
+                elif annotport < 0.25:
+                    if lowcount < low:
+                        patch = oslide.read_region(((x*pw)+1, (y*pw)+1), 0, (pw, pw));
+                        patcharr = np.array(patch)
+                        if (patcharr[:,:,3].max() == 0):
+                            #print('blank: Patch: {}_{}'.format((x*pw)+1, (y*pw)+1))
+                            continue
+                        elif whiteness(patcharr[:,:,:3]) < 12:
+                            #print('white: Patch: {}_{}'.format((x*pw)+1, (y*pw)+1))
+                            continue
+                        patch.resize((int(patch_size_40X), int(patch_size_40X)), Image.ANTIALIAS)
+                        manifest = manifest.append(pd.DataFrame({'Slide': [slidename], 'Patch': ['{}_{}'.format((x*pw)+1, (y*pw)+1)], 'Tumor': [tumor], 'Annotation': [annotport], 'Annotation Discrete': [0],
+                                                            'Native Scale': [mag]}).set_index(["Slide", "Patch"], drop=False), verify_integrity=True)
+                        patch.save('{}{}_{}_{}.png'.format(outputDir, slidename, (x*pw)+1, (y*pw)+1))
+                        lowcount += 1
+                        print('lowcount: {}, Patch: {}_{}'.format(lowcount, (x*pw)+1, (y*pw)+1))
+                else:
+                    if medcount < med:
+                        patch = oslide.read_region(((x*pw)+1, (y*pw)+1), 0, (pw, pw));
+                        patcharr = np.array(patch)
+                        if (patcharr[:,:,3].max() == 0):
+                            #print('blank: Patch: {}_{}'.format((x*pw)+1, (y*pw)+1))
+                            continue
+                        elif whiteness(patcharr[:,:,:3]) < 12:
+                            #print('white: Patch: {}_{}'.format((x*pw)+1, (y*pw)+1))
+                            continue
+                        patch.resize((int(patch_size_40X), int(patch_size_40X)), Image.ANTIALIAS)
+                        manifest = manifest.append(pd.DataFrame({'Slide': [slidename], 'Patch': ['{}_{}'.format((x*pw)+1, (y*pw)+1)], 'Tumor': [tumor], 'Annotation': [annotport], 'Annotation Discrete': [0.5],
+                                                            'Native Scale': [mag]}).set_index(["Slide", "Patch"], drop=False), verify_integrity=True)
+                        patch.save('{}{}_{}_{}.png'.format(outputDir, slidename, (x*pw)+1, (y*pw)+1))
+                        medcount += 1
+                        print('medcount: {}, Patch: {}_{}'.format(medcount, (x*pw)+1, (y*pw)+1))
+            except Exception as e:
+                print('Duplicate patch {}'.format(str(e)))
+                dupecount += 1
+                continue
         oslide.close()
-        print('extracting {}'.format(output_file));
         if remotefile:
             print("Removing {}".format(svsfile))
             os.remove(svsfile)
             remotefile = False
-    #except NameError:
-    #    missing_slides.append(slide_name)
-    #    print('Find Failed: {}'.format(slide_name))
     except Exception as e:
         print('{}: exception caught\nMessage: {}'.format(slide_name, str(e)));
+        remotefile = False
 
 
-with open('errors.txt', 'w') as f:
-    f.write('Missing Tumors: ')
-    f.write(str(missing_tumors))
-    f.write('\n')
-    f.write('Missing Slides: ')
-    f.write(str(missing_slides))
-    f.write('\n')
-
-manifest.to_csv(datafile)
+manifest.to_csv(datafile, index=False)
